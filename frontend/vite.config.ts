3b74bf613823f3eee8c14cfc0fc82e40fdf07016
Isaac <147355120+IsaacGemal@users.noreply.github.com>
2025-02-05 12:58:23 -0500

    ✨ Feat: proper pwa support (#40)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "WikiTok",
+        short_name: "WikiTok",
+        icons: [
+          {
+            src: "/wiki-logo.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+      },
+    }),
+  ],
 });
